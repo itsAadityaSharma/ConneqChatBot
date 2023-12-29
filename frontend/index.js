b31ef7f2589f3a6a970853abf1a9bb1d29de0b2f
Ashutosh Mishra <68610225+ashupn10@users.noreply.github.com>
2023-12-29 14:37:50 +0000

    Initiated frontend

---
+++ frontend/index.js
@@ -0,0 +1,26 @@
+function sendMessage() {
+    const userInput = document.getElementById('user-input');
+    const message = userInput.value.trim();
+  
+    if (message !== '') {
+      appendMessage('user', message);
+      // Here you can implement your logic to handle user input
+      // For demo, let's just clear the input field
+      userInput.value = '';
+    }
+  }
+  
+  function appendMessage(sender, message) {
+    const chatbotMessages = document.getElementById('chatbot-messages');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message', sender);
+    messageElement.innerText = message;
+    chatbotMessages.appendChild(messageElement);
+    chatbotMessages.scrollTop = chatbotMessages.scrollHeight;
+  }
+  
+  function toggleChatbot() {
+    const chatbotContainer = document.querySelector('chatbot-container');
+    chatbotContainer.classList.toggle('closed');
+  }
+  
