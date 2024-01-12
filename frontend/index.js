f823a882b7da25d434d07b77b65f978264551a9d
Raj Baghel <raj.baghel@conneqtiongroup.com>
2024-01-12 13:36:48 +0530

    all done

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,33 @@
-function sendMessage() {
+async function sendMessage() {
     const userInput = document.getElementById('user-input');
     const message = userInput.value.trim();
   
     if (message !== '') {
       appendMessage('user', message);
-      // Here you can implement your logic to handle user input
-      // For demo, let's just clear the input field
+      try{
+        const payload = {
+          "sender":"test_user",
+          "message": message
+        }
+        const response = await axios.post("http://localhost:5005/webhooks/rest/webhook",payload);
+        if(response.data){
+          appendMessage('response',response.data[0].text);
+        }else{
+          appendMessage('response',"something went wrong! Please try again later")
+        }
+      }catch(err){
+        console.log(err);
+      }
       userInput.value = '';
     }
   }
   
-  function appendMessage(sender, message) {
+  function appendMessage(sender, data) {
     const chatbotMessages = document.getElementById('chatbot-messages');
     const messageElement = document.createElement('div');
-    messageElement.classList.add('message', sender);
-    messageElement.innerText = message;
+    if(sender=='user') messageElement.classList.add('message');
+    else if(sender=='response') messageElement.classList.add('response');
+    messageElement.innerText = data;
     chatbotMessages.appendChild(messageElement);
     chatbotMessages.scrollTop = chatbotMessages.scrollHeight;
   }
